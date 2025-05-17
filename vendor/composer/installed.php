cfed76940efe0aa4b33a34fbcba83091718d27dd
Jon Lee <leeqvip@gmail.com>
2025-05-17 18:06:04 +0800

    feat: Upgrade PHP version to 8.0+
    BREAKING CHANGE: upgrade to PHP 8.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '89163d740a99aa76a9683e1cddce02da66f5ff0b',
+        'name' => 'leeqvip/database',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'leeqvip/database' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '89163d740a99aa76a9683e1cddce02da66f5ff0b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
